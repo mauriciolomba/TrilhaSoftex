fa35d9f8c1ed44ee759c50b411a11e7f19fd3fd9
mauriciolomba <mauriciolomba@hotmail.com>
2022-08-11 18:00:28 -0300

    desafio aula 3

---
+++ app/src/main/java/com/mauriciolomba/trilhasoftex/User.kt
@@ -0,0 +1,16 @@
+package com.mauriciolomba.trilhasoftex
+
+data class User(val firstName: String?,val lastName: String?)
+
+// função de extensão
+fun User.getFormattedName(): String {
+    return if (lastName != null) {
+        if (firstName != null) {
+            "$firstName $lastName"
+        } else {
+            lastName ?: "Unknown"
+        }
+    } else {
+        firstName ?: "Unknown"
+    }
+}
