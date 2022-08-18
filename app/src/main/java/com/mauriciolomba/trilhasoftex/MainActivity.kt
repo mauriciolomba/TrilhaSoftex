c2e0e93a2baf3180b8c6fb652fbcd3bcc7d813cd
mauriciolomba <mauriciolomba@hotmail.com>
2022-08-18 17:35:58 -0300

    Adding logs and enable binding

--- app/src/main/java/com/mauriciolomba/trilhasoftex/MainActivity.kt
+++ app/src/main/java/com/mauriciolomba/trilhasoftex/MainActivity.kt
@@ -2,15 +2,23 @@ package com.mauriciolomba.trilhasoftex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //binding = MainActivityBinding.inflate(layoutInflater)
+        //val view = binding.root
+        //setContentView(view)
 
         val editTextNome =  findViewById<EditText>(R.id.editTextNome)
         val editTextEmail =  findViewById<EditText>(R.id.editTextEmail)
@@ -19,11 +27,13 @@ class MainActivity : AppCompatActivity() {
         val buttonSalvar = findViewById<Button>(R.id.buttonSalvar)
 
         buttonLimpar.setOnClickListener {
+            Log.d(TAG, "Botão Limpar clicado")
             limpaCampos(editTextNome, editTextEmail, editTextTelefone)
         }
 
         buttonSalvar.setOnClickListener {
-            Toast.makeText(this, "Botão Salvar Clicado", Toast.LENGTH_SHORT).show()
+            Log.d(TAG, "Botão Salvar clicado")
+            Toast.makeText(this, "Salvo", Toast.LENGTH_SHORT).show()
             limpaCampos(editTextNome, editTextEmail, editTextTelefone)
         }
     }
